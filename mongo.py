915fe1cb32f9fe7d6f8392a5cb10fd385c25b7d1
tarahwaters <thbwaters@outlook.com>
2023-08-17 15:42:06 +0000

    Remove data for douglas using the delete_one() function and print the remaining data

--- mongo.py
+++ mongo.py
@@ -22,25 +22,7 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
-new_docs = [{
-    "first": "terry",
-    "last": "pratchett",
-    "dob": "28/04/1948",
-    "gender": "m",
-    "hair_color": "not much",
-    "occupation": "writer",
-    "nationality": "british"
-}, {
-    "first": "george",
-    "last": "rr martin",
-    "dob": "20/09/1948",
-    "gender": "m",
-    "hair_color": "white",
-    "occupation": "writer",
-    "nationality": "american"
- }]
-
-coll.insert_many(new_docs)
+coll.delete_one({"first": "douglas"})
 
 documents = coll.find()
 
